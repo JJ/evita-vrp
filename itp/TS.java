32b131ab1c164c56f10e4ce02404f9fbb431fdd2
fergunet <fergunet@ca3c812b-ca41-0410-bd8e-5b6a729be887>
2008-01-28 15:30:36 +0000

    Modificaciones en TS

--- itp/TS.java
+++ itp/TS.java
@@ -2,16 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package itp;
 
 import ec.EvolutionState;
-import ec.app.itp.ITPdata;
-import ec.app.itp.Routes4ADay;
-import ec.app.itp.VRPSolver;
+import itp.ITPdata;
+import itp.Routes4ADay;
+import itp.VRPSolver;
 import ec.app.vrp1.Route;
 import ec.app.vrp1.Shop;
 import ec.util.Parameter;
+import es.ugr.evitataboo.IncompatibleSolutionException;
 import es.ugr.evitataboo.TabuSearch;
 import es.ugr.evitataboo.impl.VRPMoveManager;
 import es.ugr.evitataboo.impl.VRPObjectiveFunction;
@@ -25,66 +25,76 @@ import java.util.List;
  * @author ferguson
  */
 public class TS extends VRPSolver {
-    
+
     private ITPdata data;
     private List<Shop> shops;
     private int thisDay;
-    
-    
     private int MAX_ITERATIONS = 100;
     private int TL_SIZE = 12;
-    
+
     public TS(EvolutionState state, Parameter base, ITPdata input, ArrayList<Shop> List, int dayOfWeek) {
-	
-	super(state, base, input, List, dayOfWeek);
+
+        super(state, base, input, List, dayOfWeek);
         this.data = input;
         this.shops = List;
         this.thisDay = dayOfWeek;
-        
+
     }
-    
+
     @Override
     public void findRoutes(EvolutionState state, int thread) {
-        
+
 
         VRPSolution initial = new VRPSolution();
-        
-        initial.setAsInitialSolution(shops, data);
-        
-        
-        
+        try {
+            initial.setAsInitialSolution(this.Depot, shops, data);
+        } catch (IncompatibleSolutionException ex) {
+            System.out.println("EXCEPTION Could not find an initial solution with" +
+                    "the given data (" + ex.getLocalizedMessage() + ")");
+            return;
+        }
+
+
+
+        //Creates the move manager
         VRPMoveManager mmanager = new VRPMoveManager();
+        //Creates the objective function
         VRPObjectiveFunction objFunc = new VRPObjectiveFunction(data);
-        
+        //Creates the tabu list
         VRPTabuList tabulist = new VRPTabuList(this.TL_SIZE);
-        
-        TabuSearch ts = new TabuSearch(initial, 
-                mmanager, 
-                objFunc, 
+
+        //Creates the Tabu Search object
+        TabuSearch ts = new TabuSearch(initial,
+                mmanager,
+                objFunc,
                 tabulist,
                 this.MAX_ITERATIONS);
-        
+
+        //Start solving
         ts.start(); //(VRPSolution)ts.getBestNeighbour(initial);
+        //Get the solution
         VRPSolution sol = (VRPSolution) ts.getBestSolution();
-        System.out.println("Solution: "+sol.toString()+" with cost "+objFunc.evaluate(sol, null));
-        
-        
-        //Construct the bestSolution
+
+        System.out.println("Solution: " + sol.toString() + " with cost " + objFunc.evaluate(sol, null));
+
+
+        //Construct the bestSolution object
         ArrayList<Integer> integerShops = new ArrayList<Integer>();
-        for(Shop s:shops){
-            Integer shopId=new Integer(Integer.parseInt(s.shopID));
+        for (Shop s : shops) {
+            Integer shopId = new Integer(Integer.parseInt(s.shopID));
             integerShops.add(shopId);
         }
-        
+
         this.bestSolution = new Routes4ADay(integerShops, this.thisDay);
         this.bestSolution.cost = objFunc.evaluate(sol, null);
-        
-        this.bestSolution.routes4Today = new ArrayList<Route>();
-        for(Route r:sol.getRoutes())
-            if(r.shopsVisited.size() > 2) //To avoid insert the routes [0, 0]
-                this.bestSolution.routes4Today.add(r);
-    }
-    
-    
 
+        this.bestSolution.routes4Today = new ArrayList<Route>();
+        for (Route r : sol.getRoutes()) {
+            if (r.shopsVisited.size() > 2) //To avoid insert the routes [0, 0]
+            {
+                this.bestSolution.routes4Today.add(r);
+            }
+
+        }
+    }
 }
